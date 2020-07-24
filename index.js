83a1f7140c6093eeb63d6acd79e25e988f7256e6
Casper <casper.deserranno@griffithuni.edu.au>
2020-07-24 13:52:38 +1000

    resolved merge conflict

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-console.log("I caused a merge conflict")
-=======
 console.log("hello world");
 
 const add = (a, b) => {return a + b};
 console.log(add(5, 10);
->>>>>>> adding-function
